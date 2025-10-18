4cacaefa0aa5eab7061406575184853de850ae0e
Finnja <g00470359@atu.ie>
2025-10-18 16:53:13 +0100

    added string definiton

--- Song.py
+++ Song.py
@@ -1,5 +1,8 @@
-class song:
+class Song:
     def __init__(self, title, artist, album):
         self.title = title
         self.artist = artist
         self.album = album
+    
+    def __str__(self):
+        return f"'{self.title}' by {self.artist} from the album '{self.album}'"
