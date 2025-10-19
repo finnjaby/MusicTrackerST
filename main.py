162e0c4974063e16b6bd77943346efa19f50e06a
Oliwier <G00434405@atu.ie>
2025-10-19 22:45:15 +0100

    Added input for songs and quit

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ from Song import Song
 
 song1 = Song("Bohemian Rhapsody", "Queen", "A Night at the Opera")
 print(song1)
+
+# list to store songs in memory
+songs = []
+
+# loop to add songs
+while True:
+    title = input("title (or 'q' to quit): ")
+    if title.lower() == "q":
+        break
+
+  
