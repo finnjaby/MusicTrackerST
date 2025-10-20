37945b313be7e9351f6b67fbb64f6fec2a1de888
IREStephen <g00465649@atu.ie>
2025-10-20 01:31:10 +0100

    Added a view_songs function to main

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from Song import Song
 song1 = Song("Bohemian Rhapsody", "Queen", "A Night at the Opera")
 print(song1)
 
+# This function views all the songs
+def view_songs(song_list):
+    print("\n=== My Music Collection ===")
+    print(f"{'Title':<25} | {'Artist':<20} | {'Album'}")
+    print("-" * 60)
+    for song in song_list:
+        print(song.display_info())
+
+# Calls the function
+view_songs(songs)
