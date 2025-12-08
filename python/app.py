60ce6b7da50f756e19a80136de7715538f97d987
Finnja <g00470359@atu.ie>
2025-12-08 03:42:18 +0000

    changed app file so users can add new songs through webpage

--- python/app.py
+++ python/app.py
@@ -1,23 +1,60 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import csv
 
 app = Flask(__name__)
 
-def load_songs_from_csv():
+CSV_FILENAME = "songs.csv"
+
+def load_songs():
     songs = []
     try:
-        with open("songs.csv", "r", newline="", encoding="utf-8") as file:
+        with open(CSV_FILENAME, "r", newline="", encoding="utf-8") as file:
             reader = csv.DictReader(file)
             for row in reader:
                 songs.append(row)
     except FileNotFoundError:
-        pass 
+        pass
     return songs
 
-@app.route('/')
-def home():
-    songs = load_songs_from_csv()
-    return render_template('web.html', songs=songs)
 
-if __name__ == '__main__':
+def save_song_to_csv(title, artist, album, genre):
+    file_exists = False
+    try:
+        open(CSV_FILENAME, "r")
+        file_exists = True
+    except:
+        pass
+
+    with open(CSV_FILENAME, "a", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+
+        # write header if file is new
+        if not file_exists:
+            writer.writerow(["title", "artist", "album", "genre"])
+
+        writer.writerow([title, artist, album, genre])
+
+
+@app.route("/")
+def home():
+    songs = load_songs()
+    return render_template("web.html", songs=songs)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_song():
+    if request.method == "POST":
+        title = request.form["title"]
+        artist = request.form["artist"]
+        album = request.form["album"]
+        genre = request.form["genre"]
+
+        save_song_to_csv(title, artist, album, genre)
+
+        return redirect("/")
+
+    return render_template("add_song.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
